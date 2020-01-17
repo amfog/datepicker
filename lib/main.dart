869463acd342b34c9dc7d4b74867336303fd6515
shapp <abdo.sf.ahmed@gmail.com>
2020-01-17 16:34:45 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+void main (){
+  runApp(new MaterialApp(home: new MyApp(),
+  ));
+}
+class MyApp extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<MyApp> {
+  String _value = '';
+  Future _selectDate() async{
+    DateTime picked = await showDatePicker(
+      context: context,
+      initialDate: new DateTime.now(),
+      firstDate: new DateTime(2016),
+      lastDate: new DateTime(2021),
+    );
+    if(picked != null)setState(() => _value = picked.toString());
+  }
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('DatePicker'),
+      ),
+      body: new Container(
+        padding: new EdgeInsets.all(32.0),
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+              new Text(_value),
+              new RaisedButton(onPressed: _selectDate, child: new Text('click me '),)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
